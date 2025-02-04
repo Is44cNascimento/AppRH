85529ba03a27d3078f8a5be777cc924247ca483e
Is44cNascimento <isaacgamex7@gmail.com>
2025-02-03 23:05:05 -0300

    subindo att 03/02

--- AppRH/src/main/java/com/AppRH/AppRH/controllers/VagaController.java
+++ AppRH/src/main/java/com/AppRH/AppRH/controllers/VagaController.java
@@ -6,6 +6,7 @@ import com.AppRH.AppRH.repository.VagaRepository;
 import jakarta.validation.Valid;
 import org.hibernate.query.NativeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +23,10 @@ import com.AppRH.AppRH.models.Vaga;
 @Controller
 public class VagaController {
 
-
+    @Autowired
     private com.AppRH.AppRH.repository.VagaRepository vr;
+
+    @Autowired
     private CandidatoRepository cr;
 
 
@@ -57,7 +60,7 @@ public class VagaController {
     @RequestMapping("/vagas")
     public ModelAndView listaVagas(){
         ModelAndView mv = new ModelAndView("vaga/listaVaga");
-        Iterable<Vaga>vagas = vr.findAll();
+        Iterable<Vaga> vagas = vr.findAll();
         mv.addObject("vagas",vagas);
 
         return mv;
@@ -79,5 +82,80 @@ public class VagaController {
 
     }
 
+    //Deletar a vaga
+
+
+    @RequestMapping( "/deletarVaga")
+    public String deletarVaga(long codigo){
+
+        Vaga vaga = vr.findByCodigo(codigo);
+        vr.delete(vaga);
+        return "redirect:/vagas";
+
+    }
+
+    public  String detalhesVagaPost(@PathVariable("codigo") long codigo, @Valid Candidato candidato,
+                                    BindingResult result, RedirectAttributes attributes){
+
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "verifique os campos");
+            return "redirect:/(codigo)";
+        }
+        //Rg duplicadp
+
+        if(cr.findByRg(candidato.getRg()) != null){
+            attributes.addFlashAttribute("mensagem erro", "Rg duplicado");
+            return "redirect:/(codigo)";
+        }
+
+
+        Vaga vaga = vr.findByCodigo(codigo);
+        candidato.setVaga(vaga);
+        cr.save(candidato);
+        attributes.addFlashAttribute("mensagem", "cadidato adicionado com sucesso !!!");
+        return "redirect:/(codigo)";
+    }
+
+    //Deleta Candidato
+
+    @RequestMapping("/deletarCandidato")
+    public String deletarCandidato (String rg){
+        Candidato candidato = cr.findByRg(rg);
+        Vaga vaga = candidato.getVaga();
+        String codigo = "" + vaga.getCodigo();
+
+
+        cr.delete(candidato);
+        return "redirect:/" + codigo;
+    }
+
+
+
+
+    //Metodos que atualizam a vaga
+    //form edicao
+
+
+
+    @RequestMapping(value = "/editar-vaga", method = RequestMethod.GET)
+    public ModelAndView editarVaga(long codigo ){
+        Vaga vaga = vr.findByCodigo(codigo);
+        ModelAndView mv = new ModelAndView("vaga/update-vaga");
+        mv.addObject("vaga", vaga);
+        return mv;
+    }
+
+    //update da vaga
+
+    @RequestMapping(value = "/editar-vaga", method = RequestMethod.POST)
+    public String updateVaga(@Valid Vaga vaga, BindingResult result, RedirectAttributes attributes){
+
+    vr.save(vaga);
+    attributes.addFlashAttribute("suucess", "vaga alterada com sucesso!!!");
+
+    long  codigoLong  = vaga.getCodigo();
+    String codigo = ""  + codigoLong;
+     return "redirect:/" + codigo;
+    }
 
 }
